274f543230de763b815a4fa497108fb84cffe09e
anibal1015 <128444208+anibal1015@users.noreply.github.com>
2023-06-12 18:50:39 -0500

    Add files via upload

---
+++ project_2687365/src/main/java/co/edu/sena/project_2687365/servlets/CategoryForwardingServlet.java
@@ -0,0 +1,19 @@
+package co.edu.sena.project_2687365.servlets;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/category")
+public class CategoryForwardingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/category.jsp");
+        dispatcher.forward(request, response);
+    }
+}
